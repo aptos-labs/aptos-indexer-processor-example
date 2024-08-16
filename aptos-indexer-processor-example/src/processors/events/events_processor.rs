f231ebbb5805ead4be96dc49a3b196373907bce7
rtso <8248583+rtso@users.noreply.github.com>
2024-08-15 18:21:28 -0700

    remove TimedBuffer

--- aptos-indexer-processor-example/src/processors/events/events_processor.rs
+++ aptos-indexer-processor-example/src/processors/events/events_processor.rs
@@ -12,10 +12,9 @@ use anyhow::Result;
 use aptos_indexer_processor_sdk::{
     aptos_indexer_transaction_stream::{TransactionStream, TransactionStreamConfig},
     builder::ProcessorBuilder,
-    common_steps::{TimedBufferStep, TransactionStreamStep},
+    common_steps::TransactionStreamStep,
     traits::IntoRunnableStep,
 };
-use std::time::Duration;
 use tracing::info;
 
 pub struct EventsProcessor {
@@ -39,17 +38,17 @@ impl EventsProcessor {
     }
 
     pub async fn run_processor(self) -> Result<()> {
-        // (Optional) Run migrations
+        // Run migrations
         run_migrations(
             self.config.db_config.postgres_connection_string.clone(),
             self.db_pool.clone(),
         )
         .await;
 
-        // (Optional) Merge the starting version from config and the latest processed version from the DB
+        // Merge the starting version from config and the latest processed version from the DB
         let starting_version = get_starting_version(&self.config, self.db_pool.clone()).await?;
 
-        // (Optional) Check and update the ledger chain id to ensure we're indexing the correct chain
+        // Check and update the ledger chain id to ensure we're indexing the correct chain
         let grpc_chain_id = TransactionStream::new(self.config.transaction_stream_config.clone())
             .await?
             .get_chain_id()
@@ -64,7 +63,6 @@ impl EventsProcessor {
         .await?;
         let events_extractor = EventsExtractor {};
         let events_storer = EventsStorer::new(self.db_pool.clone());
-        let timed_buffer = TimedBufferStep::new(Duration::from_secs(1));
         let version_tracker = LatestVersionProcessedTracker::new(
             self.config.db_config,
             starting_version,
@@ -77,7 +75,6 @@ impl EventsProcessor {
             transaction_stream.into_runnable_step(),
         )
         .connect_to(events_extractor.into_runnable_step(), 10)
-        .connect_to(timed_buffer.into_runnable_step(), 10)
         .connect_to(events_storer.into_runnable_step(), 10)
         .connect_to(version_tracker.into_runnable_step(), 10)
         .end_and_return_output_receiver(10);
@@ -93,11 +90,11 @@ impl EventsProcessor {
                         "Finished processing events from versions [{:?}, {:?}]",
                         txn_context.start_version, txn_context.end_version,
                     );
-                },
+                }
                 Err(_) => {
                     info!("Channel is closed");
                     return Ok(());
-                },
+                }
             }
         }
     }
